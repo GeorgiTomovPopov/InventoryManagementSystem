2f5b9430b245bb199016ef62a804212571bfe593
Jorata <georgi.tomov.popov@outlook.com>
2023-08-25 14:35:19 +0300

    This is just to initiate the project

---
+++ InventoryManagementSystem.java
@@ -0,0 +1,5 @@
+public class InventoryManagementSystem {
+    public static void main(String[] args) {
+        System.out.println("This is the beginning of our Inventory Management System project!");
+    }
+}
